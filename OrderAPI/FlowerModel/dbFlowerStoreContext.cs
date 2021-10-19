1d156b3a9c88758478461e414a26e2f3e13732a6
unknown <Administrator@KANINI-LTP-469.gisbizdev.gisbiz.chennai.com>
2021-10-19 17:37:23 +0530

    connection string

--- OrderAPI/FlowerModel/dbFlowerStoreContext.cs
+++ OrderAPI/FlowerModel/dbFlowerStoreContext.cs
@@ -28,7 +28,7 @@ namespace OrderAPI.FlowerModel
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=tcp:flowerstore123.database.windows.net,1433;Initial Catalog=dbFlowerStore;Persist Security Info=False;User ID=achal;Password=Flower@123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                optionsBuilder.UseSqlServer("Server=.\\SQLSERVER2019G3;Database=dbFlowerStore;Trusted_Connection=True;");
             }
         }
 
